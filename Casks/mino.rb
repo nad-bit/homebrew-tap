b14c087bb7704f61ae2adf41ab0117bc4394a58a
nad-bit <nadied@yandex.com>
2026-02-27 20:47:42 +0100

    Update SHA256 checksums for Mino cask

--- Casks/mino.rb
+++ Casks/mino.rb
@@ -2,12 +2,12 @@ cask "mino" do
   version "1.1.0"
 
   on_arm do
-    sha256 "bcddddcbb0fb2cd11a8542492e5db489d8b3c143c6259f056c44ce8cb35d9346"
+    sha256 "f6eef417053f1eef0ddb2fabffab902dea8f8f02d85e13bf280f8b1b8c615f2a"
     url "https://github.com/nad-bit/Mino/releases/download/v#{version}/Mino_v#{version}_AppleSilicon.zip"
   end
 
   on_intel do
-    sha256 "07cec13d1717185287c3f527d4cec2f51a777d9ce10ec9274ef799a7b3f5972f"
+    sha256 "6c541e70eb0040e701604904b7686b44f39354162a9d33144c365e5a2fa9abfa"
     url "https://github.com/nad-bit/Mino/releases/download/v#{version}/Mino_v#{version}_Intel.zip"
   end
 
