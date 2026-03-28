2a68c3b3577a57efc317c15065217bd3d9788d8c
NAD <nadied@yandex.com>
2026-03-28 19:30:21 +0100

    Update Mino to v1.4.8

--- Casks/mino.rb
+++ Casks/mino.rb
@@ -1,13 +1,13 @@
 cask "mino" do
-  version "1.4.7"
+  version "1.4.8"
 
   on_arm do
-    sha256 "dfcc2a5558c11b0bcc8ecb220784f906692819e02ce457f8bee67f5a42ddc244"
+    sha256 "388353acf802c0f5db24dc9158dd3fd200d0862219b1429d8e44f199c8800d8f"
     url "https://github.com/nad-bit/Mino/releases/download/v#{version}/Mino_v#{version}_AppleSilicon.zip"
   end
 
   on_intel do
-    sha256 "21ae84fce0537e43d6e55a2179c1e17f619785caa941f2aef844d217e817cdc6"
+    sha256 "171ef20a3d6e330f9ad10d62eb8e74f727a80011d0b4e016d85f6cf34a0a8e5f"
     url "https://github.com/nad-bit/Mino/releases/download/v#{version}/Mino_v#{version}_Intel.zip"
   end
 
